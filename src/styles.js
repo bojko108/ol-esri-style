c9452f0ca397962e00573b4844596a8ccbbab182
bojko108 <bojko108@gmail.com>
2022-06-13 09:24:51 +0300

    make createStyleFunction async
    - images are now loaded async and converted to ol style
    - img size is taken from arcgis server

--- src/styles.js
+++ src/styles.js
@@ -12,40 +12,45 @@ import { toRadians } from 'ol/math.js';
  * Creates a new style.
  *
  * @param {import('./types').StyleType} [styleData]
- * @return {Style}
+ * @return {Promise<Style>}
  */
-export const createFeatureStyle = (styleData) => {
-  if (!styleData.icon) {
-    styleData.icon = null;
-  }
-  if (!styleData.font) {
-    styleData.font = null;
-  }
-  if (!styleData.fill) {
-    styleData.fill = null;
-  }
-  if (!styleData.stroke) {
-    styleData.stroke = null;
-  }
-  if (!styleData.circle) {
-    styleData.circle = null;
-  }
+export const createFeatureStyle = async(styleData) => {
+    if (!styleData.icon) {
+        styleData.icon = null;
+    }
+    if (!styleData.font) {
+        styleData.font = null;
+    }
+    if (!styleData.fill) {
+        styleData.fill = null;
+    }
+    if (!styleData.stroke) {
+        styleData.stroke = null;
+    }
+    if (!styleData.circle) {
+        styleData.circle = null;
+    }
 
-  const fill = styleData.fill ? new Fill(styleData.fill) : null,
-    stroke = styleData.stroke ? new Stroke(styleData.stroke) : null,
-    fontSymbol = styleData.font ? createLabelStyle(styleData.font) : null;
-  let image = null;
+    const fill = styleData.fill ? new Fill(styleData.fill) : null,
+        stroke = styleData.stroke ? new Stroke(styleData.stroke) : null,
+        fontSymbol = styleData.font ? createLabelStyle(styleData.font) : null;
+    let image = null;
 
-  if (styleData.icon) {
-    image = new Icon(Object.assign({}, styleData.icon, { rotation: toRadians(styleData.icon.rotation || 0) }));
-  }
-  if (styleData.circle) {
-    const circleFill = styleData.circle.fill ? new Fill(styleData.circle.fill) : null;
-    const circleStroke = styleData.circle.stroke ? new Stroke(styleData.circle.stroke) : null;
-    image = new Circle({ radius: styleData.circle.radius, fill: circleFill, stroke: circleStroke });
-  }
+    if (styleData.icon) {
+        const img = await loadImage(styleData.icon.src);
+        const imgSize = [img.width, img.height];
+        const scale = styleData.icon.size[0] / img.width;
+        const rotation = toRadians(styleData.icon.rotation || 0);
+        image = new Icon(Object.assign({}, { img, imgSize, scale, rotation }));
+    }
+    if (styleData.circle) {
+        const radius = styleData.circle.radius;
+        const circleFill = styleData.circle.fill ? new Fill(styleData.circle.fill) : null;
+        const circleStroke = styleData.circle.stroke ? new Stroke(styleData.circle.stroke) : null;
+        image = new Circle({ radius, fill: circleFill, stroke: circleStroke });
+    }
 
-  return new Style({ stroke, fill, image, text: fontSymbol });
+    return new Style({ stroke, fill, image, text: fontSymbol });
 };
 
 /**
@@ -56,13 +61,21 @@ export const createFeatureStyle = (styleData) => {
  * @return {Text}
  */
 export const createLabelStyle = (labelData) => {
-  const rotation = toRadians(labelData.rotation || 0);
-  const fill = labelData.fill ? new Fill(labelData.fill) : null;
-  const stroke = labelData.stroke ? new Stroke(labelData.stroke) : null;
-  const backgroundFill = labelData.backgroundFill ? new Fill(labelData.backgroundFill) : null;
-  const backgroundStroke = labelData.backgroundStroke ? new Stroke(labelData.backgroundStroke) : null;
+    const rotation = toRadians(labelData.rotation || 0);
+    const fill = labelData.fill ? new Fill(labelData.fill) : null;
+    const stroke = labelData.stroke ? new Stroke(labelData.stroke) : null;
+    const backgroundFill = labelData.backgroundFill ? new Fill(labelData.backgroundFill) : null;
+    const backgroundStroke = labelData.backgroundStroke ? new Stroke(labelData.backgroundStroke) : null;
 
-  // text will be a template - can include: $id, {ATTRIBUTE_NAME}...
-  // before the feature is drawn on the map it will be formatted based of feature attribute values
-  return new Text(Object.assign({}, labelData, { rotation, fill, stroke, backgroundFill, backgroundStroke }));
+    // text will be a template - can include: $id, {ATTRIBUTE_NAME}...
+    // before the feature is drawn on the map it will be formatted based of feature attribute values
+    return new Text(Object.assign({}, labelData, { rotation, fill, stroke, backgroundFill, backgroundStroke }));
+};
+
+const loadImage = (imgSrc) => {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.src = imgSrc;
+    });
 };
