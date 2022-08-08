0a4c8438b697ec64df4b3b98ebff5f852a6b2529
Abraham Toriz <categulario@gmail.com>
2022-08-08 13:12:10 +0800

    adapt example to new API

--- example/index.js
+++ example/index.js
@@ -1,69 +1,40 @@
 import 'ol/ol.css';
-import { createStyleFunction, createStyleFunctionFromUrl, setMapProjection } from '../src/index.js';
+import { createStyleFunctionFromUrl } from '../src/index.js';
 
 import Map from 'ol/Map.js';
 import View from 'ol/View.js';
-import EsriJSON from 'ol/format/EsriJSON.js';
+import GeoJSON from 'ol/format/GeoJSON';
 import VectorSource from 'ol/source/Vector.js';
 import OSM from 'ol/source/OSM.js';
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
-import { tile as tileStrategy } from 'ol/loadingstrategy.js';
-import { fromLonLat } from 'ol/proj.js';
-import { createXYZ } from 'ol/tilegrid.js';
 
 window.onload = async () => {
   const legend = document.getElementById('legend-items');
 
-  const esriJsonFormat = new EsriJSON();
-  const getResolutionFromScale = (scale) => {
-    return scale / (1 * 39.37 * (25.4 / 0.28));
-  };
+  const mapServiceUrl = 'https://services3.arcgis.com/GVgbJbqm8hXASVYi/ArcGIS/rest/services/2020_Earthquakes/FeatureServer';
 
-  // const mapServiceUrl = 'https://cors-anywhere.herokuapp.com/https://sampleserver3.arcgisonline.com/ArcGIS/rest/services/Petroleum/KSFields/FeatureServer/0';
-  const mapServiceUrl = 'http://pontechbg.asuscomm.com/arcgis/rest/services/TestSymbology/MapServer';
-
-  const responce = await fetch(`${mapServiceUrl}?f=json`);
-  const mapServiceDefinition = await responce.json();
+  const response = await fetch(`${mapServiceUrl}?f=json`);
+  const mapServiceDefinition = await response.json();
   const layerDefinitions = mapServiceDefinition.layers.map((l) => ({ ...l, url: `${mapServiceUrl}/${l.id}` }));
+
+  const view = new View({
+    center: [2600708.1109138425, 5281812.509330534],
+    zoom: 5,
+  });
 
   const mapServiceLayers = layerDefinitions.map((layerDefinition) => {
     const vectorSource = new VectorSource({
-      loader: async (extent, resolution, projection) => {
-        const geometry = encodeURIComponent(
-            `{"xmin":${extent[0]},"ymin":${extent[1]},"xmax":${extent[2]},"ymax":${extent[3]},"spatialReference":{"wkid":102100}}`
-          ),
-          url = `${layerDefinition.url}/query/?f=json&returnGeometry=true&spatialRel=esriSpatialRelIntersects&geometry=${geometry}&geometryType=esriGeometryEnvelope&inSR=102100&outFields=*&outSR=102100`;
-
-        const responce = await fetch(url);
-        const responeJson = await responce.json();
-
-        if (responeJson.error) {
-          console.log(responeJson);
-        } else {
-          const features = esriJsonFormat.readFeatures(responeJson, {
-            featureProjection: projection,
-          });
-          if (features.length > 0) {
-            vectorSource.addFeatures(features);
-          }
-        }
-      },
-      strategy: tileStrategy(
-        createXYZ({
-          tileSize: 512,
-        })
-      ),
+      format: new GeoJSON(),
+      url: `${layerDefinition.url}/query?where=1%3D1&outFields=*&returnGeometry=true&f=geojson`,
     });
 
     const vector = new VectorLayer({
       source: vectorSource,
       visible: layerDefinition.defaultVisibility,
-      minResolution: getResolutionFromScale(layerDefinition.maxScale || Number.MAX_VALUE),
-      maxResolution: getResolutionFromScale(layerDefinition.minScale),
     });
     vector.setProperties({ layerDefinition });
 
-    createStyleFunctionFromUrl(layerDefinition.url).then((styleFunction) => {
+    createStyleFunctionFromUrl(layerDefinition.url, view.getProjection()).then((styleFunction) => {
       vector.setStyle(styleFunction);
     });
 
@@ -110,15 +81,8 @@ window.onload = async () => {
   const map = new Map({
     layers: [raster, ...mapServiceLayers],
     target: document.getElementById('map'),
-    view: new View({
-      //center: fromLonLat([-98.293401, 38.646303]),
-      //  center: fromLonLat([23.3, 42.7]),
-      center: [2600708.1109138425, 5281812.509330534],
-      zoom: 10,
-    }),
+    view,
   });
-
-  setMapProjection(map.getView().getProjection());
 
   window.map = map;
 };
